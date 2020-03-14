088062b9ffeb24318dace997c0691d948931901c
Julio Caballero <173180@ids.upchiapas.edu.mx>
2020-03-14 14:50:15 -0600

    project

--- practica/app/src/main/java/com/example/practica/Helpers/Helpers.java
+++
@@ -1,9 +0,0 @@
-package com.example.practica.Helpers;
-
-public class Helpers {
-    private static final String BASE_URL = "https://9d1955e5.ngrok.io/";
-
-    public static String getBaseUrl(){
-        return BASE_URL;
-    }
-}
